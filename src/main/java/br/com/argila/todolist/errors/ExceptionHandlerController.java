2ad35380d08209e7740877c9ce7e46204bd53239
Lubnnia <lubnnia@gmail.com>
2023-10-14 14:50:49 -0300

    feat: Create exception error class

---
+++ src/main/java/br/com/argila/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,17 @@
+package br.com.argila.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+// Definição de classes globais para tratamento de exceções. Toda exceção vai passar por esse controller
+public class ExceptionHandlerController {
+  
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
+  }
+}
